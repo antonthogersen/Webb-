c43e6da53f6e76acb1a9858374b052a0afeb9520
lolteson <teson02@gmail.com>
2019-05-26 21:36:42 +0200

    19-05/26

--- Uppgifter/Ugglensida/app.js
+++ Uppgifter/Ugglensida/app.js
@@ -1,11 +1,11 @@
-const linkMenu = document.querySelector('#menu');
+const linkMenu = document.querySelector('#menu'); // Tar in elementen
 const clickMenu = document.querySelector('#menuButton')
 
-clickMenu.addEventListener('click', function(openMenu){
+clickMenu.addEventListener('click', function(openMenu){ // Lägger till en eventlistener så man kan ha en click function till knappen
     
-    if(linkMenu.style.display === 'none') {
-        linkMenu.style.display = 'block'
+    if(linkMenu.style.display === 'none') { // Om den är none så
+        linkMenu.style.display = 'block'  // Gör den så det blir block
     } else {
-        linkMenu.style.display = 'none'
+        linkMenu.style.display = 'none' // Stänger av menyn
     }
 })
